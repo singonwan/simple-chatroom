1fb56ff6b76591a98b2da6d4a478aa2b41aab179
George Wan <singonwan@gmail.com>
2020-02-17 23:02:45 -0500

    added typing event which is broadcasted to other sockets, ie gwan is typing...

--- public/chat.js
+++ public/chat.js
@@ -7,6 +7,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 //Emit Events
 
@@ -16,10 +17,22 @@ btn.addEventListener('click', function() {
         message: message.value,
         handle: handle.value
     });
+    message.value = ''; //clear message after sending
+});
+// listen for keypress event
+message.addEventListener('keypress', function() {
+    socket.emit('typing', handle.value);
 });
 
 //Listen for events
+
 socket.on('chat', function(data) {
+    feedback.innerHTML = ''; //so that feedback will be empty and stop broadcasting
     output.innerHTML +=
         '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(handle) {
+    feedback.innerHTML =
+        '<p><em>' + handle + ' is typing a message...</em></p>';
 });
